37ac6727af3ae73d61cba63d6424442b98273e74
Jialei Wang <jialeiwang@JialeideMacBook-Pro.local>
2019-05-13 02:58:27 -0400

    add first user plot

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,11 @@ var postImageUrl = 'http://localhost:8888/predict/image';
 var getHistUrl = 'http://localhost:8888/history/user/';
 var postHistUrl = 'http://localhost:8888/history/user';
 
+var margin = {top: 20, right: 50, bottom: 30, left: 150},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+
 const loadResponse = () => {
     document.getElementById('result').innerHTML = 'Fetching results...';
     if (imageResponse.logo.length==0){
@@ -21,27 +26,104 @@ const loadResponse = () => {
    
 };
 
+var userLabel = [];
+var userDay = [];
+const labelCounts = Object.create(null);
+const dayCounts = Object.create(null);
+var labelD;
+var dayD;
+
 const addUserHist = history => {
-    const itemNode = document.createElement('li');
-    const textNode = document.createTextNode(
-        JSON.stringify({
-            searchResult: history.searchResult,
-            time: history.time
+    userLabel.push(...history.searchResult.label)
+    var date = new Date(history.time)
+    userDay.push(date.getDay())
+}
+
+// const addUserHist = history => {
+//     const itemNode = document.createElement('li');
+//     const textNode = document.createTextNode(
+//         JSON.stringify({
+//             searchResult: history.searchResult,
+//             time: history.time
+//         })
+//     );
+//     itemNode.appendChild(textNode);
+//     const historyList = document.getElementById('user-history');
+//     const firstNode = historyList.firstChild;
+//     historyList.insertBefore(itemNode, firstNode);
+// };
+
+const addPlot = data =>{
+    console.log(d3.max(d3.values(data)).value);
+    if (data.length === 0) {
+        d3.select("#userPage").append("h5").text("You have not searched anything.");
+    } else {
+        d3.select("#userPage").append("h5").text("It seems you like to search for ...");
+        data.sort(function(a, b) {
+          return d3.descending(a.value, b.value)
         })
-    );
-    itemNode.appendChild(textNode);
-    const historyList = document.getElementById('user-history');
-    const firstNode = historyList.firstChild;
-    historyList.insertBefore(itemNode, firstNode);
-};
+
+        var y = d3.scaleBand()
+              .range([height, 0])
+              .padding(0.1);
+
+        var x = d3.scaleLinear()
+                  .range([0, width]);
+
+        var svg = d3.select("#userPage").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+        x.domain([0,d3.max(d3.values(data)).value]);
+        y.domain(data.map(function(d) { return d.key;}));
+
+        svg.selectAll(".chartbar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "chartbar")
+          .attr("width", d => x(d.value) )
+          // function(d) { return x(d.value); }
+          .attr("y", function(d) { return y(d.key); })
+          .attr("height", y.bandwidth());
+
+        svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+        svg.append("g")
+          .call(d3.axisLeft(y));
+    }
+
+    
+}
 
 const loadUserHist = userId => {
+    userLabel = [];
+    userDay = [];
     getUserHist(userId)
         .then(histories => {
             histories.forEach(history => {
                 addUserHist(history);
             });
         })
+        .then(history => {
+            userLabel.forEach(btn => {
+                labelCounts[btn] = labelCounts[btn] ? labelCounts[btn] + 1 : 1;
+            })
+            userDay.forEach(btn => {
+                dayCounts[btn] = dayCounts[btn] ? dayCounts[btn] + 1 : 1;
+            })
+
+            labelD = d3.entries(labelCounts);
+            dayD = d3.entries(dayCounts);
+            addPlot(labelD);
+            // console.log(labelD);
+            // console.log(dayD);
+            }
+        )
         .catch(err => {
             // IMPORTANT!!! For debug only
             imageResponse = err.message;
